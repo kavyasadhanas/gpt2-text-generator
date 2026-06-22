138b9275923a8403785bda53bb787c645583ea1f
Kavya Sadhana <kavyasadhanas.it2024@citchennai.net>
2026-06-22 19:22:51 +0530

    fix vercel json

--- vercel.js
+++ vercel.js
@@ -2,10 +2,6 @@
   "version": 2,
   "rewrites": [
     {
-      "source": "/",
-      "destination": "/index.html"
-    },
-    {
       "source": "/api/generate",
       "destination": "/api/generate.js"
     }
